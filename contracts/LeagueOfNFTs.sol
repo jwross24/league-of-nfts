3dbb2c0f7584a65e057e7190e599aed2f9899aab
Jonathan Ross <jonathan.ross@caperlab.com>
2021-10-26 01:28:57 +0800

    set up character NFTs

--- contracts/LeagueOfNFTs.sol
+++ contracts/LeagueOfNFTs.sol
@@ -5,7 +5,41 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract LeagueOfNFTs {
-    constructor() {
-        console.log("This is my game contract. SWEET!");
+    struct CharacterAttributes {
+        uint characterIndex;
+        string name;
+        string imageURI;
+        uint hp;
+        uint maxHp;
+        uint attackDamage;
+        uint attackSpeed;
+        uint armor;
+    }
+
+    CharacterAttributes[] private defaultCharacters;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint[] memory characterHp,
+        uint[] memory characterAttackDmg,
+        uint[] memory characterAttackSpeed,
+        uint[] memory characterArmor
+    ) {
+        for(uint i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(CharacterAttributes({
+                characterIndex: i,
+                name: characterNames[i],
+                imageURI: characterImageURIs[i],
+                hp: characterHp[i],
+                maxHp: characterHp[i],
+                attackDamage: characterAttackDmg[i],
+                attackSpeed: characterAttackSpeed[i],
+                armor: characterArmor[i]
+            }));
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log("Done initializing %s w/ HP %s, img %s", c.name, c.hp, c.imageURI);
+        }
     }
 }
--- scripts/run.ts
+++ scripts/run.ts
@@ -2,7 +2,18 @@ import { ethers } from "hardhat";
 
 async function main() {
   const gameContractFactory = await ethers.getContractFactory("LeagueOfNFTs");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Galio", "Jinx", "Jhin"],
+    [
+      "https://rerollcdn.com/characters/Skin/6/Galio.png",
+      "https://rerollcdn.com/characters/Skin/6/Jinx.png",
+      "https://rerollcdn.com/characters/Skin/6/Jhin.png",
+    ],
+    [1800, 888, 700],
+    [150, 88, 90],
+    [6500000000, 1100000000, 900000000],
+    [70, 35, 30]
+  );
   await gameContract.deployed();
   console.log("Contract deployed to: ", gameContract.address);
 }
